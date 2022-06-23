bcf1282a4ef010419b0032d7ee277e678399cd81
deepakvit007 <72258062+deepakvit007@users.noreply.github.com>
2022-06-24 02:26:13 +0530

    Time: 4 ms (52.94%), Space: 5.9 MB (41.36%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -15,19 +15,23 @@ public:
          if(n==0||n==1)return n;
 
     
-       vector<int>dp(n+1);
+       //vector<int>dp(n+1);
         
-         dp[1]=1;
-        dp[0]=0;
-       
+//          dp[1]=1;
+//         dp[0]=0;
+       int prev1,prev2,curr;
         
-        
+        prev1=1;
+        prev2=0;
         
         for(int i=2;i<=n;i++)
         {
-         dp[i] =dp[i-1]+dp[i-2];  
+         //dp[i] =dp[i-1]+dp[i-2]; 
+            curr=prev1+prev2;
+            prev2=prev1;
+            prev1=curr;
         }
   
-        return dp[n];
+        return prev1;
     }
 };
