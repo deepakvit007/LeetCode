7c381b5dc0ed05ccef1c43c4feffcc80bd99abd2
deepakvit007 <72258062+deepakvit007@users.noreply.github.com>
2022-06-24 02:19:21 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (18.13%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    int solve(int n,int dp[n+1])
-    {
-         if(n==0||n==1)return n;
+//     int solve(int n,int dp[n+1])
+//     {
+//          if(n==0||n==1)return n;
         
-        if(dp[n]!=-1)return dp[n];
+//         if(dp[n]!=-1)return dp[n];
         
-        dp[n]=fib(n-1)+fib(n-2);
-        return dp[n];
-    }
+//         dp[n]=fib(n-1)+fib(n-2);
+//         return dp[n];
+//     }
     
     int fib(int n) {
+        
+         if(n==0||n==1)return n;
+
     
-       int dp[n+1];
-        for(int i=0;i<=n;i++)
+       vector<int>dp(n+1);
+        
+         dp[1]=1;
+        dp[0]=0;
+       
+        
+        
+        
+        for(int i=2;i<=n;i++)
         {
-         dp[i] =-1;  
+         dp[i] =dp[i-1]+dp[i-2];  
         }
-        int ans=solve(n,dp);
-        return ans;
+  
+        return dp[n];
     }
 };
