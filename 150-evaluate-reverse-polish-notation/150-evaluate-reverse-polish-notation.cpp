e57f0d02055cba60dfe4568b71326a3a00137bdf
Deepak Kumar <deepakrajmsmps880@gmail.com>
2022-07-03 00:38:01 +0530

    Time: 11 ms (83.23%), Space: 11.9 MB (61.27%) - LeetHub

--- 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
+++ 150-evaluate-reverse-polish-notation/150-evaluate-reverse-polish-notation.cpp
@@ -1,60 +1,26 @@
 class Solution {
 public:
-   int cal(int a,int b,string op)
-    {
-        if( op =="+")
-        {
-            return a + b;
-        }
-        if(op =="-")
-        {
-            return b - a;
-        }
-        if(op =="*")
-        {
-            return a * b;
-        }
-        if(op =="/")
-        {
+   int evalRPN(vector<string>& tokens) {
+      stack<int>s;
+        
+        for(const string& str : tokens){            
+            if(str == "+" || str == "-" || str == "*" || str == "/"){
+                
+                int right = s.top();
+                s.pop();
+                int left =  s.top(); 
+                s.pop();
             
-            return b/a;
+                if(str == "+")      s.push((left+right));
+                else if(str == "-") s.push((left-right));
+                else if(str == "*") s.push((left*right));
+                else                s.push((left/right));
+                
+            } else 
+                s.push(stoi(str)); // convert string to int
         }
-        return 0;
-    }
-    
-    int evalRPN(vector<string>& tokens) {
-        int n=tokens.size();
-        if(n==1) return stoi(tokens[0]);
-        if(n<2) return 0;
-     stack<string>ans;
         
-        int temp1,temp2,res;
-        
-        
-        for(int i=0;i<n;i++)
-        {
-           
-            if(tokens[i]=="+"||tokens[i]=="*"||tokens[i]=="-"||tokens[i]=="/")
-            {
-                temp1=stoi(ans.top());
-                cout<<temp1<<endl;
-                ans.pop();
-                temp2=stoi(ans.top());
-                cout<<temp2<<endl;
-                ans.pop();
-                res=cal(temp1,temp2,tokens[i]);
-                cout<<res<<endl;
-                ans.push(to_string(res));
-            }
-             else
-            {
-                ans.push(tokens[i]);
-            }
-            
-        }
-        int anss=stoi(ans.top());
-        
-        return anss ;
+        return s.top(); 
         
     }
 };
