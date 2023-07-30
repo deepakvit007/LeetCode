f0a92768d65953c082345c841d4967162c119551
Deepak Kumar <deepakrajmsmps880@gmail.com>
2023-07-30 14:44:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (45.24%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int solve(int i,int j,vector<int>& nums)
+    int solve(int i,int j,vector<int>& nums,vector<vector<int>>&dp)
     {
         if(i>j)return 0;
 
         if(i == j)return nums[i];
+        if(dp[i][j]!=-1)return dp[i][j];
+        int takei = nums[i] + min(solve(i+2,j,nums,dp),solve(i+1,j-1,nums,dp));
+        int takej = nums[j] + min(solve(i+1,j-1,nums,dp),solve(i,j-2,nums,dp));
 
-        int takei = nums[i] + min(solve(i+2,j,nums),solve(i+1,j-1,nums));
-        int takej = nums[j] + min(solve(i+1,j-1,nums),solve(i,j-2,nums));
-
-        return max(takei,takej);
+        return dp[i][j] = max(takei,takej);
     }
     bool PredictTheWinner(vector<int>& nums) {
         int n =  nums.size();
-
+         vector<vector<int>>dp(n,vector<int>(n,-1));
         int sum = accumulate(nums.begin(),nums.end(),0);
-        int player1 = solve(0,n-1,nums);
+        int player1 = solve(0,n-1,nums,dp);
 
         int player2 = sum - player1;
 
@@ -23,9 +23,6 @@ public:
 
 
         return true;
-        
-
-
 
     }
 };
