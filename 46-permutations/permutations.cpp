d259d2d349e32a1e6b43e6f008bed272910cfc81
Deepak Kumar <deepakrajmsmps880@gmail.com>
2023-08-03 12:31:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (46.69%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+        void solve(int index,vector<int>ip,vector<vector<int>> &ans)
+    {
+        if(index==ip.size()-1)
+        {
+            ans.push_back(ip);
+            return;
+        }
+        
+      for(int i=index;i<ip.size();i++)
+      {
+          swap(ip[index],ip[i]);
+          solve(index+1,ip,ans);
+          //backtrack
+          swap(ip[index],ip[i]);
+      }
+        
+        
+        
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+         
+        vector<vector<int>>ans;
+        vector<int>op;
+        solve(0,nums,ans);
+        return ans;
+    }
+};
