55b4d59629754f4fdee68d42499c6fbee3888177
Deepak Kumar <deepakrajmsmps880@gmail.com>
2023-08-16 01:08:01 +0530

    Time: 8 ms (38.01%) | Memory: 10.4 MB (21.46%) - LeetSync

--- 86-partition-list/partition-list.cpp
+++ 86-partition-list/partition-list.cpp
@@ -1,66 +1,66 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    void insertAtEnd(ListNode*& head, ListNode * newNode)
-    {
-        
-        if(head == NULL)
+class Solution
+{
+    public:
+        void insertAtEnd(ListNode* &head, ListNode *newNode)
         {
-            head = newNode;
-        }
-        else
-        {
-        ListNode * temp = head;
-        while(temp->next!=NULL)
-        {
-            temp = temp->next;
-        }
 
-        temp->next = newNode;
-        }
-
-    }
-    ListNode* partition(ListNode* head, int x) {
-        if(head==NULL )return NULL;
-        if(head->next==NULL )return head;
-        ListNode * list1 = NULL;
-        ListNode * list2 = NULL;
-        
-        ListNode* temp = head;
-        ListNode* prev = NULL;
-
-        while(temp!=NULL)
-        {
-            ListNode * newNode = new ListNode(temp->val);
-            if(temp->val < x )
+            if (head == NULL)
             {
-                insertAtEnd(list1,newNode);
+                head = newNode;
+            }
+            else
+            {
+                ListNode *temp = head;
+                while (temp->next != NULL)
+                {
+                    temp = temp->next;
+                }
+
+                temp->next = newNode;
+            }
+        }
+    ListNode* partition(ListNode *head, int x)
+    {
+        if (head == NULL) return NULL;
+
+        ListNode *list1 = NULL;
+        ListNode *list2 = NULL;
+
+        ListNode *temp = head;
+        ListNode *prev = NULL;
+
+        while (temp != NULL)
+        {
+            ListNode *newNode = new ListNode(temp->val);
+            if (temp->val < x)
+            {
+                insertAtEnd(list1, newNode);
                 prev = newNode;
             }
-            else if(temp->val>=x)
+            else if (temp->val >= x)
             {
-                insertAtEnd(list2,newNode);
+                insertAtEnd(list2, newNode);
             }
 
             temp = temp->next;
         }
 
-        if(list1!=NULL)
-        {prev->next = list2;
-        return list1;
+        if (list1 != NULL)
+        {
+            prev->next = list2;
+            return list1;
         }
 
         return list2;
-
     }
-
 };
