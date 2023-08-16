ee337fe1a46bce2d7adb00620ac6124547c13032
Deepak Kumar <deepakrajmsmps880@gmail.com>
2023-08-17 00:51:02 +0530

    Time: 317 ms (90.93%) | Memory: 161 MB (57.15%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -10,41 +10,17 @@
  */
 class Solution {
 public:
-
-        ListNode* reverse(ListNode* head) {
-        if(head==NULL)return head;
-        ListNode * prev = NULL;
-        ListNode * curr = head;//5
-        ListNode * nex = head->next;//4
-
-        while(curr!=NULL)
-        {
-            curr->next = prev;
-
-            prev = curr;
-            curr = nex;
-            if(nex!=NULL)nex = nex->next;
-
-        }
-       head=prev;
-       return head;
-    }
     ListNode* removeNodes(ListNode* head) {
-        head = reverse(head);
-        ListNode*ptr = head;
-        while(head->next!=NULL)
+        
+        if(head == NULL)return NULL;
+
+        head->next = removeNodes(head->next);
+        
+        if(head->next!=NULL and head->val < head->next->val)
         {
-            if(head->val > head->next->val)
-            {
-                head->next = head->next->next;
-            }
-            else
-            {
-                head=head->next;
-            }
+            return head->next;
         }
 
-        head = reverse(ptr);
         return head;
     }
 };
