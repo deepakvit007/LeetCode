bd32a03e01c345d9e3f26f1720c905fbb3f774f4
Deepak Kumar <deepakrajmsmps880@gmail.com>
2023-08-17 11:26:27 +0530

    Time: 173 ms (82.37%) | Memory: 74.1 MB (22.12%) - LeetSync

--- 148-sort-list/sort-list.cpp
+++ 148-sort-list/sort-list.cpp
@@ -1,79 +1,80 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
+ *Definition for singly-linked list.
+ *struct ListNode {
+ *    int val;
+ *    ListNode * next;
+ *    ListNode() : val(0), next(nullptr) {}
+ *    ListNode(int x) : val(x), next(nullptr) {}
+ *    ListNode(int x, ListNode *next) : val(x), next(next) {}
+ *};
  */
-class Solution {
-public:
-    pair<ListNode*,ListNode*> middle (ListNode * head)
+class Solution
+{
+    public:
+    pair<ListNode*, ListNode*> middle(ListNode *head)
     {
-        ListNode * slow = head;
-        ListNode * fast = head;
-        ListNode * prev = NULL;
+        ListNode *slow = head;
+        ListNode *fast = head;
+        ListNode *prev = NULL;
 
-        while(fast!=NULL and fast->next!=NULL)
-        {   
+        while (fast != NULL and fast->next != NULL)
+        {
             prev = slow;
             slow = slow->next;
             fast = fast->next->next;
         }
         prev->next = NULL;
+        //return two list 1. head to prev and 2. slow to end of list
         return {head,slow};
     }
-    ListNode* merge(ListNode* list1, ListNode* list2) {
-        
+    ListNode* merge(ListNode *list1, ListNode *list2)
+    {
+
         ListNode *start = new ListNode(0);
         ListNode *tail = start;
 
-        if(list1==NULL)return list2;
-        if(list2==NULL)return list1;
+        if (list1 == NULL) return list2;
+        if (list2 == NULL) return list1;
 
-        while(list1!=NULL and list2!=NULL)
+        while (list1 != NULL and list2 != NULL)
         {
-            if(list1->val <= list2->val)
+            if (list1->val <= list2->val)
             {
                 tail->next = list1;
                 tail = list1;
                 list1 = list1->next;
             }
-            else if(list1->val > list2->val)
+            else if (list1->val > list2->val)
             {
                 tail->next = list2;
                 tail = list2;
                 list2 = list2->next;
             }
-
-            
         }
 
-        if(list1==NULL)tail->next = list2;
+        if (list1 == NULL) tail->next = list2;
         else
-        tail->next = list1;
+            tail->next = list1;
 
         return start->next;
-
     }
-    ListNode* mergeSort(ListNode* head)
+    ListNode* mergeSort(ListNode *head)
     {
-        if(head == NULL || head->next == NULL)return head ;
-
+        //base case
+        if (head == NULL || head->next == NULL) return head;
+        
+        //dividing into two linked list
         auto mid = middle(head);
-        ListNode *l1 = mid.first;
-        ListNode *l2 = mid.second;
-
-       l1 =  mergeSort(l1);
-        l2 = mergeSort(l2);
-
-        return merge(l1,l2);
+        
+        //sorting the two divided linked list
+        ListNode *l1 = mergeSort(mid.first);
+        ListNode *l2 = mergeSort(mid.second);
+        
+        //merging the two divided linked list
+        return merge(l1, l2);
     }
-    ListNode* sortList(ListNode* head) {
+    ListNode* sortList(ListNode *head)
+    {
         return mergeSort(head);
-
-    
     }
 };
