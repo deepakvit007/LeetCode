6ceb7f4b85db516446fd529a6f0537648daaff5b
Deepak Kumar <deepakrajmsmps880@gmail.com>
2023-08-25 00:08:56 +0530

    Time: 25 ms (51.39%) | Memory: 27.7 MB (51.04%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -11,22 +11,26 @@
  */
 class Solution {
 public:
-    int solve(TreeNode* root,int &res)
+    int solve(TreeNode* &root,int &res)
     {
          if(root == NULL)return 0;
 
-         int left = solve(root->left , res);
+         int left = solve(root->left,res);
          int right = solve(root->right,res);
+         
+         //edge case when both left and right subtree return negative value
+         // so here we not include them in our answer 
          if(left < 0 and right < 0) {left = 0;right=0;}
+          
+         //we send maxAns possible to next node 
          int dusre_ke_liye_ans = root->val + max(left , right);
-
+         
+         //we assume that node will be the ans
          int me_khud_ko_ans_manogaa = max(dusre_ke_liye_ans , root->val + left + right);
          
-         cout<<dusre_ke_liye_ans<<" ";
-         cout<<me_khud_ko_ans_manogaa<<"x";
+         //store the ans;
          res = max(res , me_khud_ko_ans_manogaa);
-         cout<<res;
-
+        
          return  dusre_ke_liye_ans ;
     }
     int maxPathSum(TreeNode* root) {
