30d05c6291d6e56f7a65185ef4f0b99a86e77ff9
Deepak Kumar <deepakrajmsmps880@gmail.com>
2024-03-26 00:24:37 +0530

    Time: 33 ms (90.81%) | Memory: 35.9 MB (90.94%) - LeetSync

---
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+void swapSort(vector<int>& nums)
+    {
+        int i=0;
+        int n=nums.size();
+        while(i<n)
+        {
+            int correct = nums[i]-1;
+
+            if(nums[i]!=nums[correct])
+            swap(nums[i],nums[correct]);
+            else
+            i++;
+        }
+    }
+    vector<int> findDuplicates(vector<int>& nums) {
+         vector<int>ans;
+        int n = nums.size();
+        swapSort(nums);
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=i+1)
+            ans.push_back(nums[i]);
+        }
+        return ans;
+    }
+};
