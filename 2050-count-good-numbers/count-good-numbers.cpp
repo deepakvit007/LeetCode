a3b7f4304a3eb306c7f86bf815e2c114cf92c359
Deepak Kumar <deepakrajmsmps880@gmail.com>
2024-07-04 22:05:29 +0530

    Time: 0 ms (100.00%) | Memory: 7.2 MB (47.95%) - LeetSync

--- 2050-count-good-numbers/count-good-numbers.cpp
+++ 2050-count-good-numbers/count-good-numbers.cpp
@@ -22,20 +22,19 @@ class Solution
     }
     int countGoodNumbers(long long n)
     {
-        if (n == 1) return 5;
+        // if (n % 2 == 1)
+        // {
+        //     oddPlace = n / 2;
+        //     evenPlace = n / 2 + 1;
+        // }
+        // else
+        // {
+        //     oddPlace = n / 2;
+        //     evenPlace = n / 2;
+        // }
 
-        long long evenPlace = 0;
-        long long oddPlace = 0;
-        if (n % 2 == 1)
-        {
-            oddPlace = n / 2;
-            evenPlace = n / 2 + 1;
-        }
-        else
-        {
-            oddPlace = n / 2;
-            evenPlace = n / 2;
-        }
+        long long evenPlace = n/2 + n%2;
+        long long oddPlace = n/2;
 
         return (pow(5, evenPlace) *pow(4, oddPlace)) % mod;
     }
