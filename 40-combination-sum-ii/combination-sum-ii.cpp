abcdcad22d43c7c0f9d2a1789ee8008516f20f87
Deepak Kumar <deepakrajmsmps880@gmail.com>
2024-07-06 01:26:25 +0530

    Time: 9 ms (22.72%) | Memory: 12.4 MB (97.01%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,39 +1,33 @@
-class Solution {
-public:
-    void solve (int ind,vector<int>ip,vector<int>op,vector<vector<int>>&ans,int k,int sum)
-    {
-         if(sum==k)
+class Solution
+{
+    public:
+        void solve(int ind, vector<int> &input, vector<int> &output, int target, vector< vector< int>> &ans)
         {
-            ans.push_back(op);
-            return;
-            
-        }
-        if(ind>=ip.size()||sum>k)
-        {
-            return;
-        }
-        
-       
-      op.push_back(ip[ind]);
-         sum+=ip[ind];
-       solve(ind+1,ip,op,ans,k,sum);
-        op.pop_back();    
-        sum-=ip[ind];
-           while(ind+1<ip.size() and ip[ind] == ip[ind+1])ind++;
-        solve(ind+1,ip,op,ans,k,sum); 
-        
-      
-    
-    }
-    
-    vector<vector<int>> combinationSum2(vector<int>& ip, int target) {
-         
-        vector<vector<int>>ans2;
-        vector<int>op;
-        int sum=0;
+            if (target == 0)
+            {
+                ans.push_back(output);
+                return;	//very imp
+            }
+            if (ind >= input.size() || target < 0)
+            {
+                return;
+            }
 
-        sort(ip.begin(),ip.end());
-        solve(0,ip,op,ans2,target,sum);
-        return ans2;
+           	//take
+            output.push_back(input[ind]);
+            solve(ind + 1, input, output, target - input[ind], ans);
+            output.pop_back();
+            while (ind + 1 < input.size() and input[ind] == input[ind + 1]) ind++;
+           	//not take
+            solve(ind + 1, input, output, target, ans);
+        }
+    vector<vector < int>> combinationSum2(vector<int> &nums, int target)
+    {
+        vector<vector < int>> ans;
+        vector<int> output;
+        sort(nums.begin(), nums.end());
+        solve(0, nums, output, target, ans);
+
+        return ans;
     }
 };
