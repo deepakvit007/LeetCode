7c1918f6e1596fae7adec4f67e2bb1d7d863e23a
Deepak Kumar <deepakrajmsmps880@gmail.com>
2024-08-13 01:33:51 +0530

    Time: 4 ms (60.98%) | Memory: 14.9 MB (68.05%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.cpp
@@ -1,48 +1,46 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *    int val;
+ *    TreeNode * left;
+ *    TreeNode * right;
+ *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
  */
-class Solution {
-public:
-    vector<vector<int>> levelOrder(TreeNode* root) {
-
-        if(root  == NULL)return {};
-
-        queue<TreeNode*>q;
-        q.push(root);
-        
-         vector<vector<int>>ans; 
-        while(!q.empty())
+class Solution
+{
+    public:
+        vector<vector < int>> levelOrder(TreeNode *root)
         {
-            int size = q.size();
-             vector<int>temp;
-            while(size--)
-            {
-                auto node = q.front();
-                q.pop();
-                temp.push_back(node->val);
+            if (root == NULL) return {};
 
-                if(node->left !=NULL)
-                {
-                    q.push(node->left);
-                }
-               if(node->right!=NULL)
-               {
-                q.push(node->right);
-               }
-            }
+    queue<pair<TreeNode*, int>> q;
+    q.push({root, 0});
+    vector<vector<int>> ans;
 
-            ans.push_back(temp);
+    while (!q.empty()) {
+        auto [node, level] = q.front();
+        q.pop();
+
+        // Ensure ans has enough space for the current level
+        if (ans.size() <= level) {
+            ans.push_back({});
         }
 
-        return ans;
+        // Add the current node's value to the corresponding level
+        ans[level].push_back(node->val);
 
+        // Enqueue children with the level incremented by 1
+        if (node->left != NULL) {
+            q.push({node->left, level + 1});
+        }
+        if (node->right != NULL) {
+            q.push({node->right, level + 1});
+        }
     }
+
+    return ans;
+        }
 };
