308004c9548913d75dea851b8d6ae39e2d0ea380
Deepak Kumar <deepakrajmsmps880@gmail.com>
2024-10-07 02:13:07 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (80.82%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -55,8 +55,30 @@ public:
 
     }
     int uniquePaths(int m, int n) {
-        // vector<vector<int>> dp(m, vector<int>(n, -1));
-        // return solveMemo(m-1,n-1,dp);
-        return solveTab(m,n);
+        
+        vector<int>prev(m,0);
+        vector<int>curr(n,0);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i == 0 and j == 0)curr[j] = 1;
+
+                else
+                {
+                    int left = 0;
+                    int up = 0;
+
+                    if(i>0)left = prev[j];
+                    if(j>0)up = curr[j-1];
+
+                    curr[j] = up + left;
+                }
+               prev = curr;
+
+            }
+        }
+
+        return prev[n-1];
     }
 };
