e2171b34013097c4fb15f253216eab9905bbfb50
Deepak Kumar <deepakrajmsmps880@gmail.com>
2024-11-08 01:21:57 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (6.92%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -9,29 +9,42 @@ class Solution {
         
     }
 
-    public int fib_memo(int n ) {
-        int dp[] = new int[n+1]; // first step - 1 extra 
-
-        // 2nd step ka fill -1 
-        Arrays.fill(dp,-1);
+    public int fib_memo(int n , int dp[]) {
 
         if(n<=1)return n;
 
         if(dp[n]!=-1) return dp[n]; // checking if we already calculated the values or not 
 
-        return dp[n] = fib_memo(n-1)+fib_memo(n-2); // stroing the answer
+        return dp[n] = fib_memo(n-1, dp)+fib_memo(n-2, dp); // stroing the answer
         
     }
 
+
+
     public int fib(int n) {
+        //n=1;
+        if(n<=1)return n; // 
 
         // conversion to memoziation
         //1. array bana 
 
+        int dp[] = new int[n+1]; // first step - 1 extra 
 
+        // 2nd step ka fill -1 
+        Arrays.fill(dp,-1);
 
+ 
+        // base case handle 
 
-        return fib_memo(n);
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2 ; i<=n ; i++)
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
         
     }
 
