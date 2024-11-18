cbcbbc57007dc48b4f46ecb5915abadf20222b64
Deepak Kumar <deepakrajmsmps880@gmail.com>
2024-11-19 01:48:07 +0530

    Time: 9 ms (12.50%) | Memory: 52.2 MB (14.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -2,7 +2,7 @@ class Pair {
     int first;
     int second;
 
-    Pair(int first, int second) {
+    public Pair(int first, int second) {
         this.first = first;
         this.second = second;
     }
@@ -16,26 +16,23 @@ class Solution {
         Queue<Pair> q = new LinkedList<Pair>();
         q.add(new Pair(i, j));
 
-        int delRow [] = {1,0,-1,0};
-        int delCol [] = {0,-1,0,1};
-
+        int delRow[] = { 1, 0, -1, 0 };
+        int delCol[] = { 0, -1, 0, 1 };
 
         while (!q.isEmpty()) {
             int row = q.peek().first;
             int col = q.peek().second;
             q.remove();
 
-            for (int k= 0; k <4; k++) {
-                
-                    int newRow = row + delRow[k];
-                    int newCol = col + delCol[k];
+            for (int k = 0; k < 4; k++) {
+                int newRow = row + delRow[k];
+                int newCol = col + delCol[k];
 
-                    // Check valid, unvisited land cell
-                    if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol] == '1' && !visited[newRow][newCol]) {
-                        q.add(new Pair(newRow, newCol));
-                        visited[newRow][newCol] = true;
-                    }
-                
+                // Check valid, unvisited land cell
+                if (newRow >= 0 && newRow < grid.length && newCol >= 0 && newCol < grid[0].length && grid[newRow][newCol] == '1' && !visited[newRow][newCol]) {
+                    q.add(new Pair(newRow, newCol));
+                    visited[newRow][newCol] = true;
+                }
             }
         }
     }
@@ -46,16 +43,23 @@ class Solution {
 
         boolean visited[][] = new boolean[n][m];
 
-        int count = 0;
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < m; col++) {
-                if (!visited[row][col] && grid[row][col] == '1') {
-                    count++;
-                    bfs(row, col, visited, grid);
-                }
-            }
-        }
+        AtomicInteger count = new AtomicInteger(0);
 
-        return count;
+        IntStream
+            .range(0, n)
+            .forEach(
+                i ->
+                    IntStream
+                        .range(0, m)
+                        .forEach(
+                            j -> {
+                                if (!visited[i][j] && grid[i][j] == '1') {
+                                    count.incrementAndGet();
+                                    bfs(i, j, visited, grid);
+                                }
+                            }
+                        )
+            );
+        return count.get();
     }
 }
