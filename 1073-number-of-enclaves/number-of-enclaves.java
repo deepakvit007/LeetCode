2e51e8f9a8ff434032088dc1c5d2e1fd5da2cb93
Deepak Kumar <deepakrajmsmps880@gmail.com>
2024-11-23 21:39:46 +0530

    Time: 11 ms (19.54%) | Memory: 54.9 MB (67.97%) - LeetSync

--- 1073-number-of-enclaves/number-of-enclaves.java
+++ 1073-number-of-enclaves/number-of-enclaves.java
@@ -1,48 +1,79 @@
+class Pair{
+
+    int row;
+    int col;
+
+    public Pair(int r, int c){
+
+        this.row = r;
+        this.col = c;
+
+    }
+
+}
 class Solution {
 
      // Depth-First Search (DFS) function to mark connected 'O' cells
-    public void dfs(int row, int col, boolean visited[][], int [][] board, int n, int m) {
+    public void bfs(boolean visited[][], int [][] board, int n, int m) {
         // Mark the current cell as visited
-        visited[row][col] = true;
+         // Step 1: Perform DFS for all 'O' cells on the boundaries
+     
 
         // Define possible directions to move: down, left, up, right
         int delRow[] = {1, 0, -1, 0};
         int delCol[] = {0, -1, 0, 1};
+        
+        Queue<Pair>q = new LinkedList<>();
+       
 
-        // Traverse all 4 possible directions
-        for (int i = 0; i < 4; i++) {
-            int nRow = row + delRow[i];
-            int nCol = col + delCol[i];
-
-            // Check if the next cell is within bounds, not visited, and contains 'O'
-            if (nRow >= 0 && nCol >= 0 && nRow < n && nCol < m &&
-                !visited[nRow][nCol] && board[nRow][nCol] == 1) {
-                // Recursively call DFS on the connected cell
-                dfs(nRow, nCol, visited, board, n, m);
-            }
-        }
-    }
-
-    public int numEnclaves(int[][] board) {
-
-        // Get dimensions of the board
-        int n = board.length;        // Number of rows
-        int m = board[0].length;     // Number of columns
-
-        // Create a visited array to keep track of explored cells
-        boolean visited[][] = new boolean[n][m];
-
-        // Step 1: Perform DFS for all 'O' cells on the boundaries
         for (int i = 0; i < n; i++) {       // Traverse rows
             for (int j = 0; j < m; j++) {   // Traverse columns
                 // If the cell is on the boundary and contains 'O', start DFS
                 if (i == 0 || j == 0 || i == n - 1 || j == m - 1) {
                     if (!visited[i][j] && board[i][j] == 1 ) {
-                        dfs(i, j, visited, board, n, m);
+                        q.add(new Pair(i,j));
+                        visited[i][j] = true;
                     }
                 }
             }
         }
+
+        while(!q.isEmpty())
+        {
+            int row = q.peek().row;
+            int col = q.peek().col;
+
+            q.remove();
+
+            
+            // Traverse all 4 possible directions
+            for (int i = 0; i < 4; i++) {
+                int nRow = row + delRow[i];
+                int nCol = col + delCol[i];
+
+                // Check if the next cell is within bounds, not visited, and contains 'O'
+                if (nRow >= 0 && nCol >= 0 && nRow < n && nCol < m &&
+                    !visited[nRow][nCol] && board[nRow][nCol] == 1) {
+                        // Recursively call DFS on the connected cell
+                    visited[nRow][nCol] = true;
+                    q.add(new Pair(nRow,nCol));
+
+                }
+            }
+        }
+
+    }
+
+    public int numEnclaves(int[][] board) {
+           // Get dimensions of the board
+        int n = board.length;        // Number of rows
+        int m = board[0].length;     // Number of columns
+
+        // Create a visited array to keep track of explored cells
+        boolean visited[][] = new boolean[n][m];
+        
+        bfs(visited,board,n,m);
+       
         int ans = 0;
 
         // Step 2: Flip all unvisited 'O' cells to 'X'
